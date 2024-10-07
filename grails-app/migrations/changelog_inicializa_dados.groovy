3b5f8c43809f9e672289e0da96f7b30e1e635cb2
eduardo <eduardo@gmail.com>
2024-10-07 16:31:00 -0300

    Commit Inicial do Projeto

--- grails-app/migrations/changelog_inicializa_dados.groovy
+++ grails-app/migrations/changelog_inicializa_dados.groovy
@@ -71,12 +71,16 @@ databaseChangeLog = {
 		grailsChange {
 			change {
 				sql.execute("""
-				COPY usuario (id,version,email_port,start_ssl,sistema,ativar_email,corretor_id,password_expired,account_expired,ultimo_login,email_password,carteira_id,start_tls,username,email_server,email_username,account_locked,password,loja_id,ultimo_bloqueio,pessoa_id,email_ssl,enabled,email_remetente,ultimo_login_ip) FROM '/pinedu/modelo/usuario.csv' with csv header;
-				COPY grupo (id,version,sistema,descricao,nome) FROM '/pinedu/modelo/grupo.csv' with csv header;
+				COPY regra (id,version,sistema,descricao,authority) FROM '/pinedu/modelo/regra.csv' with csv header NULL 'NULL';
+				COPY usuario (id,version,email_port,start_ssl,sistema,ativar_email,corretor_id,password_expired,account_expired,ultimo_login,email_password,carteira_id,start_tls,username,email_server,email_username,account_locked,password,loja_id,ultimo_bloqueio,pessoa_id,email_ssl,enabled,email_remetente,ultimo_login_ip) FROM '/pinedu/modelo/usuario.csv' with csv header NULL 'NULL';
+				COPY requestmap (id,version,config_attribute,url) FROM '/pinedu/modelo/requestmap.csv' with csv header NULL 'NULL';
+				COPY usuario_regra (user_id,role_id) FROM '/pinedu/modelo/usuario_regra.csv' with csv header NULL 'NULL';
+				COPY grupo (id,version,sistema,descricao,nome) FROM '/pinedu/modelo/grupo.csv' with csv header NULL 'NULL';
+				COPY grupo_regra (role_id,grupo_id) FROM '/pinedu/modelo/grupo_regra.csv' with csv header NULL 'NULL';
 				COPY menu (id,version,sistema,icone,mneumonic,nome,selected,dica,aplicacao_id,pai_id,posicao,tipo,administrativo,alinhamento,\"action\",enabled) FROM '/pinedu/modelo/menu.csv' with csv header NULL 'NULL';
-				COPY grupo_menu (grupo_id,menu_id) FROM '/pinedu/modelo/grupo_menu.csv' with csv header;
-				COPY usuario_grupo (user_id,grupo_id) FROM '/pinedu/modelo/usuario_grupo.csv' with csv header;
-				COPY usuario_menu (user_id,menu_id) FROM '/pinedu/modelo/usuario_menu.csv' with csv header;
+				COPY grupo_menu (grupo_id,menu_id) FROM '/pinedu/modelo/grupo_menu.csv' with csv header NULL 'NULL';
+				COPY usuario_grupo (user_id,grupo_id) FROM '/pinedu/modelo/usuario_grupo.csv' with csv header NULL 'NULL';
+				COPY usuario_menu (user_id,menu_id) FROM '/pinedu/modelo/usuario_menu.csv' with csv header NULL 'NULL';
 				""")
 			}
 		}
@@ -118,7 +122,6 @@ databaseChangeLog = {
 						, setval('seq_aplicacao', (SELECT MAX( id ) FROM aplicacao))
 						, setval('seq_bairro', (SELECT MAX( id ) FROM bairro))
 						, setval('seq_bairro_corret', (SELECT MAX( id ) FROM bairro_corretagem))
-						, setval('seq_banco', (SELECT MAX( id ) FROM banco))
 						, setval('seq_campanha', (SELECT MAX( id ) FROM campanha))
 						, setval('seq_carteira_imovel', (SELECT MAX( id ) FROM carteira_imovel))
 						, setval('seq_cidade', (SELECT MAX( id ) FROM cidade))
@@ -127,7 +130,6 @@ databaseChangeLog = {
 						, setval('seq_classe_interessado', (SELECT MAX( id ) FROM classe_interessado))
 						, setval('seq_concorrente', (SELECT MAX( id ) FROM concorrente))
 						, setval('seq_condominio', (SELECT MAX( id ) FROM condominio))
-						, setval('seq_contrato_exclusividade', (SELECT MAX( id ) FROM contrato_exclusividade))
 						, setval('seq_corretor', (SELECT MAX( id ) FROM corretor))
 						, setval('seq_edificio', (SELECT MAX( id ) FROM edificio))
 						, setval('seq_empresa', (SELECT MAX( id ) FROM empresa))
